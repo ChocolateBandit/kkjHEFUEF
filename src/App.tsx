35a1ec567a932871636488a41f3e31edef492f51
ChocolateBandit <kadyrkulove@mail.ru>
2023-11-14 21:02:42 +0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,58 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Salam</h1>
-        <p>Lorem</p>
+        <AppTitle></AppTitle>
+        <Rating></Rating>
+        <Accordion></Accordion>
     </div>
   );
+}
+
+function AppTitle () {
+    return <h1>This is app component</h1>
+}
+// function hello() {
+//     debugger
+//     alert("Helloo");
+// };
+// hello();
+// const App = () => {
+//     return (
+//         <div>
+//             This is other function
+//             <Rating></Rating>
+//             <Accordion></Accordion>
+//         </div>
+//     )
+// }
+
+function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+
+        </div>
+    )
+}
+
+function Star() {
+    return (
+       <span>Star </span>
+    )
+}
+
+
+function Accordion() {
+    return <div>
+        <h3>Menu</h3>
+        <ul>
+            <li>2</li>
+            <li>1</li>
+            <li>3</li>
+        </ul>
+    </div>
 }
 
 export default App;
